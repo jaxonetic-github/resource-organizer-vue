c446df0e9a28945c54e6a8b29418d88d796afd4a
Alonzo <jaxonetic@gmail.com>
2018-01-10 07:56:43 -0800

    updated to load and import my components instead of default HelloWorld.vue app

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import Vuetify from 'vuetify'
+import CityDirectory from './components/city-directory'
+import DetailDisplay from './components/detail-display.vue'
+import SummaryDisplay from './components/summary-display.vue'
+import {siteTestData} from './util.js'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
+
+// Globally registering 2 sub components
+Vue.component('detail-display', DetailDisplay)
+Vue.component('summary-display', SummaryDisplay)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  data: function () { return {siteData: '', debug: true} },
+  created: function () { this.siteData = siteTestData() },
+  components: {'city-directory': CityDirectory}
 })
